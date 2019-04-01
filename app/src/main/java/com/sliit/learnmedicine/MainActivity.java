e9c9f9354a490338f9137634b48eec4b72863dab
Ssameer3079 <smrbasil4@gmail.com>
2019-04-01 23:17:43 +0530

    Implemented Navigating to Next Activity

--- app/src/main/java/com/sliit/learnmedicine/MainActivity.java
+++ app/src/main/java/com/sliit/learnmedicine/MainActivity.java
@@ -1,7 +1,10 @@
 package com.sliit.learnmedicine;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListAdapter;
 import android.widget.ListView;
@@ -18,12 +21,20 @@ public class MainActivity extends AppCompatActivity {
 
         initializeComponents();
 
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                startActivity(new Intent(getApplicationContext(), ViewMedicine.class));
+            }
+        });
+
         String[] foods = {"Mesalazine", "Acetaminophen", "Amoxicillin", "Hydrocodone",
-        "Haldol", "Hytrin", "Hydrochlorothiazide", "Codeine", "Lisinopril", "Adderall", "Acetaminophen",
-        "Oxycodone", "Prednisone"};
-        ListAdapter adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, foods);
+                "Haldol", "Hytrin", "Hydrochlorothiazide", "Codeine", "Lisinopril", "Adderall", "Acetaminophen",
+                "Oxycodone", "Prednisone"};
+        ListAdapter adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, foods);
 
         listView.setAdapter(adapter);
+
     }
 
     private void initializeComponents() {
