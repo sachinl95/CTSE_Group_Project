5214027c9f4a05ae8817cd5cbc60279146c026d4
Ssameer3079 <smrbasil4@gmail.com>
2019-04-01 23:02:19 +0530

    Implemented a list view

--- app/src/main/java/com/sliit/learnmedicine/MainActivity.java
+++ app/src/main/java/com/sliit/learnmedicine/MainActivity.java
@@ -2,12 +2,35 @@ package com.sliit.learnmedicine;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListAdapter;
+import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
+
+    ListView listView;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        initializeComponents();
+
+        String[] foods = {"Mesalazine", "Acetaminophen", "Amoxicillin", "Hydrocodone",
+        "Haldol", "Hytrin", "Hydrochlorothiazide", "Codeine", "Lisinopril", "Adderall", "Acetaminophen",
+        "Oxycodone", "Prednisone"};
+        ListAdapter adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, foods);
+
+        listView.setAdapter(adapter);
+    }
+
+    private void initializeComponents() {
+        listView = findViewById(R.id.listView);
+    }
+
+    private void getMedicines() {
+
     }
 }
